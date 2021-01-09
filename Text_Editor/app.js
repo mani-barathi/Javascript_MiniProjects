3cb93fb3c40ee4c84625994f642c10aa429d5775
mani-barathi <manibarathi.s@gmail.com>
2021-01-09 08:57:16 +0530

    Design Done for Text Editor

---
+++ Text_Editor/app.js
@@ -0,0 +1,10 @@
+const titleEl = document.querySelector('.header__title')
+const editorEl = document.querySelector('.editor')
+const saveBtn = document.querySelector('.header__btnSave')
+const downloadBtn = document.querySelector('.header__btnDownload')
+
+
+
+
+
+editorEl.focus()
